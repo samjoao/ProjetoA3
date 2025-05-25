b43661db91340137ad7d0e0247d6a2450574e606
SamJoao <santesvitor@gmail.com>
2025-05-24 23:33:47 -0300

    att2

--- Js/script.js
+++ Js/script.js
@@ -9,7 +9,7 @@
 
     // Função para validar email simples
     function isValidEmail(email) {
-      const re = /^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$/;
+      const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
       return re.test(email);
     }
 
