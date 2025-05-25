48866725b0ec9755aae4e09ac66faeb881708e51
SamJoao <santesvitor@gmail.com>
2025-05-25 18:02:43 -0300

    SERVERNOVO

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
-const databaseUrl = process.env.DATABASE_Url;
+const databaseUrl = process.env.DATABASE_URL;
 
 const allowedOrigins = [
     'http://localhost:8080',
